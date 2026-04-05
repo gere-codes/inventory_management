ef7a8acad14758e42a22cf6d6ae49af57533ae8e
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-05 01:05:48 -0400

    add the refresh and logout routes

--- api-node/src/modules/auth/auth.route.ts
+++ api-node/src/modules/auth/auth.route.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import { authConroller } from './auth.controller.js';
+import { verifyRefreshToken } from './auth.middleware.js';
 
 export const authRoutes = Router();
 
 authRoutes.post('/register', authConroller.register);
 authRoutes.post('/login', authConroller.login);
+authRoutes.post('/refresh', verifyRefreshToken, authConroller.refresh);
+authRoutes.post('/logout', authConroller.logout);

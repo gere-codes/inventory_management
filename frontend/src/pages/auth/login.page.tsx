c4a9188a5b10140d7beff3417c7edc9c319afcb8
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-05 18:22:32 -0400

    create login form

---
+++ frontend/src/pages/auth/login.page.tsx
@@ -0,0 +1,51 @@
+import { useForm } from 'react-hook-form';
+import { Button, InputField } from '@ui/index';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { type TLoginFormData, loginSchema } from '@/features/auth/auth.schema';
+
+export const LoginPage = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<TLoginFormData>({
+		resolver: zodResolver(loginSchema),
+		mode: 'all',
+	});
+
+	console.log(errors);
+	const onSubmit = (data: TLoginFormData) => {
+		console.log('Form data:', data);
+	};
+
+	return (
+		<form
+			onSubmit={handleSubmit(onSubmit)}
+			className="max-w-md mx-auto p-4 flex items-center justify-center h-screen"
+		>
+			<div className="flex flex-col gap-4 w-full">
+				<h2 className="font-bold text-4xl text-center">Login</h2>
+				<InputField
+					{...register('email')}
+					label="Email"
+					id="email"
+					type="email"
+					placeholder="your@mail.com"
+					error={errors.email?.message as string}
+				/>
+
+				<InputField
+					{...register('password')}
+					label="Password"
+					id="password"
+					type="password"
+					placeholder="........"
+					error={errors.password?.message as string}
+				/>
+				<Button variant="primary" type="submit">
+					Login
+				</Button>
+			</div>
+		</form>
+	);
+};

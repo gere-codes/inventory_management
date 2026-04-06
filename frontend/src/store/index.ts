3d6dd829c50d0dfa20053c7f6cb1b4f06b2f5450
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-06 00:00:53 -0400

    create redux hooks

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -22,5 +22,5 @@ export const store = configureStore({
 
 export const persistor = persistStore(store);
 
-export type RootState = ReturnType<typeof rootReducer>;
-export type AppDispatch = ReturnType<typeof store.dispatch>;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;

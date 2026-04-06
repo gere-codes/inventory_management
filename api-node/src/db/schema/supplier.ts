b847e7e3a065b9304423b250fd7a950b2ddce704
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-06 14:13:19 -0400

    create the suppliers schama

---
+++ api-node/src/db/schema/supplier.ts
@@ -0,0 +1,16 @@
+import { pgTable, uuid, varchar, timestamp } from 'drizzle-orm/pg-core';
+import { users } from './user.js';
+
+export const suppliers = pgTable('suppliers', {
+	id: uuid('id').primaryKey().defaultRandom(),
+	userId: uuid('user_id').references(() => users.id),
+	name: varchar('name', { length: 100 }).notNull(),
+	phone: varchar('phone', { length: 36 }),
+	address: varchar('address', { length: 225 }),
+	description: varchar('description', { length: 225 }),
+	createdAt: timestamp('created_at').defaultNow().notNull(),
+	updatedAt: timestamp('updated_at')
+		.defaultNow()
+		.notNull()
+		.$onUpdate(() => new Date()),
+});

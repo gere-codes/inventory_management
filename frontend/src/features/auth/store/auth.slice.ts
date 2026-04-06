09dce49b941e59800aad6d9e004f37d8c1d2f4ae
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-05 23:40:00 -0400

    add the register thunk cases into the auth slice

---
+++ frontend/src/features/auth/store/auth.slice.ts
@@ -0,0 +1,44 @@
+import type { TUser } from '@/shared/schema';
+import { createSlice } from '@reduxjs/toolkit';
+import { authThunk } from '@auth/store/auth.thunks';
+
+export type AuthState = {
+	user: TUser;
+	isAuthenticated: boolean;
+	status: 'idle' | 'loading' | 'succeeded' | 'failed';
+	error: string | null;
+};
+
+const initialState: AuthState = {
+	user: {} as TUser,
+	isAuthenticated: false,
+	error: null,
+	status: 'idle',
+};
+
+export const authSlice = createSlice({
+	name: 'auth',
+	initialState,
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			// register
+			.addCase(authThunk.register.pending, (state) => {
+				state.status = 'loading';
+			})
+			.addCase(authThunk.register.fulfilled, (state, action) => {
+				state.user = action.payload;
+				state.isAuthenticated = true;
+				state.status = 'succeeded';
+			})
+			.addCase(authThunk.register.rejected, (state, action) => {
+				state.isAuthenticated = false;
+				state.status = 'failed';
+				if (action.error.message) {
+					state.error = action.payload as string;
+				} else {
+					state.error = 'Register failed';
+				}
+			});
+	},
+});

8a89e4579e246eb8f8baa8d116a86e77aed045c5
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-06 00:31:02 -0400

    import the redux persist storage from /es instead of from /lib

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,23 +1,28 @@
-import { combineReducers } from 'redux';
-import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
 import { authSlice } from '@auth/store/auth.slice';
 
 const persistConfig = {
 	key: 'root',
 	storage,
+	version: 1,
 };
 
 const rootReducer = combineReducers({
-	auth: authSlice,
+	auth: authSlice.reducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
 	reducer: persistedReducer,
-	middleware: (getDefaultMiddlware) => getDefaultMiddlware({ serializableCheck: false }),
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 });
 
 export const persistor = persistStore(store);

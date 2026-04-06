e5c03663e22f373b7e4dfe7623f740d891107a97
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-06 14:13:06 -0400

    create the product schema

---
+++ api-node/src/db/schema/product.ts
@@ -0,0 +1,18 @@
+import { pgTable, uuid, varchar, timestamp, decimal, integer } from 'drizzle-orm/pg-core';
+import { users } from './user.js';
+import { categories } from './category.js';
+
+export const products = pgTable('products', {
+	id: uuid('id').primaryKey().defaultRandom(),
+	userId: uuid('user_id').references(() => users.id),
+	name: varchar('name', { length: 100 }).notNull(),
+	description: varchar('description', { length: 255 }),
+	sku: varchar('sku', { length: 36 }).unique().notNull(),
+	price: decimal('price').default('0.00').notNull(),
+	quantity: integer('quantity').default(1),
+	categoryId: uuid('category_id').references(() => categories.id),
+	createdAt: timestamp('created_at').defaultNow().notNull(),
+	updatedAt: timestamp('updated_at')
+		.defaultNow()
+		.$onUpdate(() => new Date()),
+});

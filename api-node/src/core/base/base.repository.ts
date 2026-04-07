b3bce68cf42aabbc193f9b245b579e6294946bc4
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-07 10:36:08 -0400

    implement findById funciton to the base repository

--- api-node/src/core/base/base.repository.ts
+++ api-node/src/core/base/base.repository.ts
@@ -1,15 +1,17 @@
 import type { PaginatedResult } from '@core/types/index.js';
-import { eq, type AnyTable, type ColumnBaseConfig } from 'drizzle-orm';
+import { and, eq, type AnyTable, type ColumnBaseConfig } from 'drizzle-orm';
 import type { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import type { AnyPgTable, PgColumn, PgTable, PgTableWithColumns } from 'drizzle-orm/pg-core';
-import z from 'zod';
+import z, { record } from 'zod';
 
 export interface IBaseRepository<T, TCreate, TUpdate> {
 	findAll(userId: string): Promise<T[]>;
+	findById(userId: string, id: string): Promise<T | null>;
 }
 
 type AnyUserIdColumn = PgColumn<ColumnBaseConfig<'string', string>>;
-type TableWithUserId = PgTable<any> & { userId: AnyUserIdColumn };
+type AnyIdColumn = PgColumn<ColumnBaseConfig<'string', string>>;
+type TableWithUserId = PgTable<any> & { userId: AnyUserIdColumn; id: AnyIdColumn };
 
 export abstract class BaseRepository<T, TCreate, TUpdate, TTable extends TableWithUserId> implements IBaseRepository<
 	T,
@@ -36,10 +38,28 @@ export abstract class BaseRepository<T, TCreate, TUpdate, TTable extends TableWi
 		this.db = db;
 	}
 
+	protected abstract format(record: any): T;
+
 	async findAll(userId: string): Promise<T[]> {
-		return (await this.db
+		const records = (await this.db
 			.select()
 			.from(this.table as AnyPgTable)
 			.where(eq(this.table.userId, userId))) as T[];
+
+		const formattedRecords = records.map((rec) => this.format(rec));
+		return z.array(this.schema).parse(formattedRecords);
+	}
+
+	async findById(userId: string, id: string): Promise<T | null> {
+		const record = await this.db
+			.select()
+			.from(this.table as AnyPgTable)
+			.where(and(eq(this.table.id, id), eq(this.table.userId, userId)))
+			.limit(1);
+
+		if (record.length === 0) return null;
+
+		const formattedRecord = this.format(record[0]);
+		return this.schema.parse(formattedRecord);
 	}
 }

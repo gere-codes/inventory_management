59a3f898aff04b64c35f43bdaa76c2c05b46b0d1
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-08 22:35:01 -0400

    setup the interceptors

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,9 @@ import { BrowserRouter } from 'react-router';
 import { Provider } from 'react-redux';
 import { store, persistor } from '@store/index.ts';
 import { PersistGate } from 'redux-persist/integration/react';
+import { privateInstance, refreshClient } from './shared/api/instance.api.ts';
+import { authServices } from './features/auth/auth.service.ts';
+import { authThunk } from './features/auth/store/auth.thunks.ts';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
@@ -17,4 +20,42 @@ createRoot(document.getElementById('root')!).render(
 			</Provider>
 		</BrowserRouter>
 	</StrictMode>,
+);
+
+privateInstance.interceptors.request.use(
+	async (config) => {
+		const token = await authServices.getFreshToken(refreshClient);
+
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+
+		return config;
+	},
+	(error) => Promise.reject(error),
+);
+
+// res interceptor
+privateInstance.interceptors.response.use(
+	(response) => response,
+	async (error) => {
+		const originalRequest = error.config;
+
+		if (error.response?.status === 401 && !originalRequest._retry) {
+			originalRequest._retry = true;
+
+			try {
+				const newToken = await authServices.getFreshToken(refreshClient);
+
+				originalRequest.headers.Authorization = `Bearer ${newToken}`;
+				return privateInstance(originalRequest);
+			} catch (refreshError) {
+				store.dispatch(authThunk.logout());
+				authServices.setAccessToken('');
+				return Promise.reject(refreshError);
+			}
+		}
+
+		return Promise.reject(error);
+	},
 );

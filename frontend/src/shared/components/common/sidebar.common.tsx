96892bbc119798a5958f8114daae8c81f8e1826a
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-09 21:09:19 -0400

    add logo

--- frontend/src/shared/components/common/sidebar.common.tsx
+++ frontend/src/shared/components/common/sidebar.common.tsx
@@ -1,7 +1,11 @@
+import { SiGoogletagmanager } from 'react-icons/si';
+
 export const SideBar = () => {
 	return (
-		<section className="flex flex-row grap-2 px-4 py-2 min-w-[250px]">
-			<h1>sidebar</h1>
+		<section className="flex flex-row grap-2 px-4 py-2 min-w-[250px] bg-primary rounded">
+			<h1 className="text-white">
+				<SiGoogletagmanager size={35} />
+			</h1>
 		</section>
 	);
 };

3e152b3c503ff4b2bb3ad876940243569b5a65db
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-10 12:57:10 -0400

    implement the base service functions

--- frontend/src/shared/base/base.service.ts
+++ frontend/src/shared/base/base.service.ts
@@ -4,14 +4,15 @@ import z from 'zod';
 
 export interface IBaseService<T, TCreate, TUpdate> {
 	getAll(): Promise<T[]>;
-	getById(id: string | number): Promise<T>;
+	getById(id: string): Promise<T>;
 	create(data: TCreate): Promise<T>;
-	update(id: string | number, data: TUpdate): Promise<T>;
-	delete(id: string | number): Promise<T>;
+	update(id: string, data: TUpdate): Promise<T>;
+	delete(id: string): Promise<T>;
 	paginate(page: number, limit: number): Promise<PaginatedResult<T>>;
+	search(term: string, page: number, limit: number): Promise<PaginatedResult<T>>;
 }
 export abstract class BaseService<T, TCreate, TUpdate> implements IBaseService<T, TCreate, TUpdate> {
-	protected apiUrl: string;
+	protected resource: string;
 	protected schema: z.ZodSchema<T>;
 	protected createSchema: z.ZodSchema<TCreate>;
 	protected updateSchema: z.ZodSchema<TUpdate>;
@@ -22,9 +23,65 @@ export abstract class BaseService<T, TCreate, TUpdate> implements IBaseService<T
 		createSchema: z.ZodSchema<TCreate>,
 		updateSchema: z.ZodSchema<TUpdate>,
 	) {
-		this.apiUrl = resource;
+		this.resource = resource;
 		this.schema = schema;
 		this.createSchema = createSchema;
 		this.updateSchema = updateSchema;
+	}
+	async getAll(): Promise<T[]> {
+		const result = await privateInstance.get<{ success: boolean; data: T[] }>(`/${this.resource}`);
+		return z.array(this.schema).parse(result.data.data);
+	}
+
+	async getById(id: string): Promise<T> {
+		const result = await privateInstance.get<{ success: boolean; data: T }>(`/${this.resource}/${id}`);
+		return this.schema.parse(result.data.data);
+	}
+
+	async create(body: TCreate): Promise<T> {
+		const result = await privateInstance.post<{ success: boolean; data: T }>(`/${this.resource}`, body);
+		return this.schema.parse(result.data.data);
+	}
+
+	async update(id: string, body: TUpdate): Promise<T> {
+		const result = await privateInstance.put<{ success: boolean; data: T }>(`/${this.resource}/${id}`, body);
+		return this.schema.parse(result.data.data);
+	}
+
+	async delete(id: string): Promise<T> {
+		const result = await privateInstance.delete<{ success: boolean; data: T }>(`/${this.resource}/${id}`);
+		return this.schema.parse(result.data.data);
+	}
+
+	async paginate(page: number, limit: number): Promise<PaginatedResult<T>> {
+		const result = await privateInstance.get<{ success: boolean; data: PaginatedResult<T> }>(
+			`/${this.resource}/paginate`,
+			{
+				params: {
+					currentPage: page,
+					itemsPerPage: limit,
+				},
+			},
+		);
+
+		return {
+			data: z.array(this.schema).parse(result.data.data.data),
+			pagination: result.data.data.pagination,
+		};
+	}
+
+	async search(term: string, page: number, limit: number): Promise<PaginatedResult<T>> {
+		const result = await privateInstance.get(`/${this.resource}/search`, {
+			params: {
+				term,
+				currentPage: page,
+				itemsPerPage: limit,
+			},
+		});
+
+		return {
+			data: z.array(this.schema).parse(result.data.data.data),
+			pagination: result.data.data.pagination,
+		};
 	}
 }

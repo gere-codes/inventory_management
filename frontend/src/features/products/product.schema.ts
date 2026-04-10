ca35bf61cade2c658c576bc3fb8dd1163befe41b
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-09 21:44:11 -0400

    create product shema

---
+++ frontend/src/features/products/product.schema.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod';
+import { EProductStatus } from './product.enum.js';
+
+export const productCreateSchema = z.object({
+	name: z.string().min(2).max(100),
+	price: z.coerce.number().positive(),
+	description: z.string().max(1000).nullable().optional(),
+	quantity: z.number().int().nonnegative(),
+	categoryId: z.uuid(),
+	sku: z.string().min(3).max(36),
+});
+
+export const productSchema = productCreateSchema.extend({
+	id: z.uuid(),
+	category: z.string(),
+	status: z.enum([EProductStatus.IN_STOCK, EProductStatus.LOW_STOCK, EProductStatus.OUT_OF_STOCK]),
+	createdAt: z.coerce.date(),
+	updatedAt: z.coerce.date(),
+});
+export const productUpdateSchema = productCreateSchema.partial();
+
+export type TProduct = z.infer<typeof productSchema>;
+export type TProductCreate = z.infer<typeof productCreateSchema>;
+export type TProductUpdate = z.infer<typeof productUpdateSchema>;

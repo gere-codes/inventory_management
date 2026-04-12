67e2621b5c1a4016800ad0525a02a62db5cec67d
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-11 23:17:48 -0400

    create the suppliers page

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,2 +1,4 @@
 export * from './auth';
 export * from './products/products.page';
+export * from './suppliers.tsx';
+export * from './categories/categories.page.tsx';
---
+++ frontend/src/pages/suppliers.tsx/suppliers.page.tsx
@@ -0,0 +1,7 @@
+export const SuppliersPage = () => {
+	return (
+		<section>
+			<h2>section page</h2>
+		</section>
+	);
+};

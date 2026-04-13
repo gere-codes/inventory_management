ad38915b732d954697ca350f689f96569be385f5
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-13 17:26:00 -0400

    feat(category): add the search barm category table, and pagination to the category page

--- frontend/src/pages/categories/categories.page.tsx
+++ frontend/src/pages/categories/categories.page.tsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from 'react';
+import { selectCategories, selectCategoryPagination, categoryThunk, CategoryTable } from '@categories';
+import { useAppDispatch, useAppSelector } from '@hooks/index';
+import { Pagination, SearchBar } from '@common/index';
+
 export const CategoriesPage = () => {
+	const [term, setTerm] = useState<string>('');
+
+	const dispatch = useAppDispatch();
+	const categories = useAppSelector(selectCategories);
+
+	const { currentPage, itemsPerPage, totalItems, totalPages } = useAppSelector(selectCategoryPagination);
+
+	useEffect(() => {
+		dispatch(categoryThunk.paginate({ page: currentPage, limit: itemsPerPage }));
+	}, [dispatch, currentPage, itemsPerPage]);
+
+	const noop = () => {};
+
 	return (
 		<section>
-			<h2>categories page</h2>
+			<SearchBar value={term} onSearch={(newValue) => setTerm(newValue)} placeholder="Search Product..." />
+			<CategoryTable categories={categories} onDelete={noop} onEdit={noop} />
+			<Pagination
+				currentPage={currentPage}
+				totalPages={totalPages}
+				itemsPerPage={itemsPerPage}
+				onPageChange={noop}
+				onPerPageChange={noop}
+			/>
 		</section>
 	);
 };

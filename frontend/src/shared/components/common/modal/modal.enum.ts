fe1c57f2e84f8e6116fee6bddf0352d5820a7ced
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-16 13:36:27 -0400

    feat(modal): add the modal enums

---
+++ frontend/src/shared/components/common/modal/modal.enum.ts
@@ -0,0 +1,14 @@
+export enum EModalType {
+	PRODUCT = 'PRODUCT',
+	SUPPLIER = 'SUPPLIER',
+	ORDER = 'ORDER',
+	CATEGORY = 'CATEOGRY',
+	RESET = 'RESET',
+}
+
+export enum EModalMode {
+	CREATE = 'CREATE',
+	EDIT = 'EDIT',
+	VIEW = 'VIEW',
+	DELETE = 'DELETE',
+}

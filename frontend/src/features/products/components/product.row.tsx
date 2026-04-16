2f038ef5a27dd1fb778974e7c093e2b3a9e4aec0
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-15 21:53:10 -0400

    style(products row): set the status table data padding to 1.5

--- frontend/src/features/products/components/product.row.tsx
+++ frontend/src/features/products/components/product.row.tsx
@@ -27,7 +27,7 @@ export const ProductItem = memo(({ product, onEdit, onDelete, onOrder }: Props) 
 			<TableItemData item={product.category} />
 			<TableItemData item={`${product.quantity} pcs`} />
 			<td className="px-4 py-4  w-1/7 text-ellipsis overflow-hidden">
-				<span className={`p-2 text-xs truncate font-medium rounded ${statusStyles[product.status]}`}>
+				<span className={`px-3 py-1.5 text-xs truncate font-medium rounded ${statusStyles[product.status]}`}>
 					{product.status}
 				</span>
 			</td>

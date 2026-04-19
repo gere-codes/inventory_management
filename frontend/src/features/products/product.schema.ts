9583e81b27aa0c391b3f48fa6e0b56bd78209e3f
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-19 16:15:08 -0400

    feat(product schema): add a discriminated union schema for the  product form

--- frontend/src/features/products/product.schema.ts
+++ frontend/src/features/products/product.schema.ts
@@ -19,6 +19,34 @@ export const productSchema = productCreateSchema.extend({
 });
 export const productUpdateSchema = productCreateSchema.partial();
 
+export const productFormSchema = z.discriminatedUnion('mode', [
+	// CREATE
+	z.object({
+		mode: z.literal('CREATE'),
+		name: z.string().min(2),
+		price: z.number().min(0.01),
+		categoryId: z.uuid(),
+		id: z.string().optional(),
+		sku: z.string().min(3, 'Minimum 3 charatesr are required'),
+		quantity: z.number().min(0, 'Quantity'),
+		description: z.string().optional().nullable(),
+	}),
+
+	// EDIT
+	z.object({
+		mode: z.literal('EDIT'),
+		name: z.string().min(2),
+		price: z.number().min(0.01),
+		categoryId: z.uuid(),
+		id: z.uuid(),
+		sku: z.string().min(3, ' SKU must be minium 3 characters'),
+		quantity: z.number().min(0, 'Quantity'),
+		description: z.string().optional().nullable(),
+	}),
+]);
+
 export type TProduct = z.infer<typeof productSchema>;
 export type TProductCreate = z.infer<typeof productCreateSchema>;
-export type TProductUpdate = z.infer<typeof productSchema>;
+export type TProductUpdate = z.infer<typeof productUpdateSchema>;
+
+export type TProductFormValues = z.infer<typeof productFormSchema>;

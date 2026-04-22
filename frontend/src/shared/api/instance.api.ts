171b15216995c40948febc5f5a9b07b4f75e7ca8
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-22 18:48:51 -0400

    update(api): update the base api instance

--- frontend/src/shared/api/instance.api.ts
+++ frontend/src/shared/api/instance.api.ts
@@ -1,30 +1,21 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_BASE || 'http://localhost:5000/api';
+export const BASE_URL = import.meta.env.VITE_BASE || 'http://localhost:5000';
 
 // public instance
 export const publicInstance = axios.create({
-	baseURL: `${BASE_URL}`,
+	baseURL: `${BASE_URL}/api`,
 	withCredentials: true,
-	headers: {
-		'Content-Type': 'application/json',
-	},
 });
 
 // private instance
 export const privateInstance = axios.create({
-	baseURL: `${BASE_URL}`,
+	baseURL: `${BASE_URL}/api`,
 	withCredentials: true,
-	headers: {
-		'Content-Type': 'application/json',
-	},
 });
 
 // refresh token instance
 export const refreshClient = axios.create({
-	baseURL: `${BASE_URL}`,
+	baseURL: `${BASE_URL}/api`,
 	withCredentials: true,
-	headers: {
-		'Content-Type': 'application/json',
-	},
 });

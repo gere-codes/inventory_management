3dfcb13f82e5fedcd9998fe355b79e812fb9cbdd
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-22 23:18:41 -0400

    feat(product route): add the upload multer middleware to the update product route

--- api-node/src/modules/product/product.route.ts
+++ api-node/src/modules/product/product.route.ts
@@ -7,7 +7,7 @@ const productRoutes = Router();
 productRoutes.get('/', productController.getAll);
 productRoutes.get('/paginate', productController.paginate);
 productRoutes.post('/', upload.single('image'), productController.create);
-productRoutes.put('/:id', productController.update);
+productRoutes.put('/:id', upload.single('image'), productController.update);
 productRoutes.delete('/:id', productController.delete);
 
 productRoutes.get('/search', productController.search);

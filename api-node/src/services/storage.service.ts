eb89da92f0e269a1d929b80d14cdb6ee191e7e57
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-22 23:50:30 -0400

    feat(image middleware): implement a function that deletes image

--- api-node/src/services/storage.service.ts
+++ api-node/src/services/storage.service.ts
@@ -1,3 +1,7 @@
+import { AppError } from '@src/core/utils/app-error.util.js';
+import { unlinkSync, unlink } from 'fs';
+import { join } from 'path';
+
 export interface IFileService {
 	upload(file: Express.Multer.File): Promise<string>;
 }
@@ -5,5 +9,16 @@ export interface IFileService {
 export class LocalFileService implements IFileService {
 	async upload(file: Express.Multer.File): Promise<string> {
 		return `/uploads/${file.filename}`;
+	}
+
+	async delete(filename: string): Promise<void> {
+		if (!filename) return;
+
+		try {
+			const imagePath = join(process.cwd(), 'uploads', filename);
+			await unlinkSync(imagePath);
+		} catch (error) {
+			throw new AppError(404, 'File not found');
+		}
 	}
 }

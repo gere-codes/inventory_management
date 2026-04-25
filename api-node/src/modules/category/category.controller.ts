e5fe4c1f3fede1ab120813618589a7390e10b5d2
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-24 23:49:14 -0400

    feat(category controller): add ICategoryService generic parameter to BaseController

--- api-node/src/modules/category/category.controller.ts
+++ api-node/src/modules/category/category.controller.ts
@@ -2,9 +2,9 @@ import { BaseController } from '@src/core/base/base.controller.js';
 import type { TCategory, TCategoryCreate, TCategoryUpdate } from './category.schema.js';
 import { CategoryRepository } from './category.repository.js';
 import { db } from '@src/db/index.js';
-import { CategoryService } from './category.service.js';
+import { CategoryService, type ICategoryService } from './category.service.js';
 
-class CategoryController extends BaseController<TCategory, TCategoryCreate, TCategoryUpdate> {
+class CategoryController extends BaseController<TCategory, TCategoryCreate, TCategoryUpdate, ICategoryService> {
 	constructor() {
 		const repo = new CategoryRepository(db);
 		const service = new CategoryService(repo);

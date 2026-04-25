b0348760f677ec2f4cdcb34baddaee276c6b7c9d
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-24 23:50:31 -0400

    feat(category repository): add ICategoryRepository interface and impelemnt it to the category repository

--- api-node/src/modules/category/category.repository.ts
+++ api-node/src/modules/category/category.repository.ts
@@ -1,11 +1,14 @@
-import { BaseRepository } from '@src/core/base/base.repository.js';
+import { BaseRepository, type IBaseRepository } from '@src/core/base/base.repository.js';
 import { categories, db } from '@src/db/index.js';
 import type { TCategory, TCategoryCreate, TCategoryUpdate } from './category.schema.js';
 import type { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import type { PaginatedResult } from '@src/core/types/general.js';
 import { and, desc, eq, ilike, or, sql, SQL, type SQLWrapper } from 'drizzle-orm';
-
-export class CategoryRepository extends BaseRepository<TCategory, TCategoryCreate, TCategoryUpdate, typeof categories> {
+export interface ICategoryRepository extends IBaseRepository<TCategory, TCategoryCreate, TCategoryUpdate> {}
+export class CategoryRepository
+	extends BaseRepository<TCategory, TCategoryCreate, TCategoryUpdate, typeof categories>
+	implements ICategoryRepository
+{
 	constructor(db: NodePgDatabase<any>) {
 		super(categories, db);
 	}

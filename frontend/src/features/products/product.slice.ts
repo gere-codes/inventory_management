28ae7e86df09c6804c81e97fbc5578026aa19d07
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-27 13:14:09 -0400

    feat(product slice): addd the  getStats product thunk to the product slice

--- frontend/src/features/products/product.slice.ts
+++ frontend/src/features/products/product.slice.ts
@@ -1,5 +1,27 @@
 import { baseSlice } from '@base';
 import { productThunk } from './product.thunk';
+import type { TProduct } from './product.schema';
+import { createIinitialBaseState } from '@constants';
 
-export const productSlice = baseSlice('product', productThunk, {});
+export const productSlice = baseSlice(
+	'product',
+	productThunk,
+	{ ...createIinitialBaseState<TProduct>(), stats: null },
+	{},
+
+	(builder) => {
+		builder
+			.addCase(productThunk.getStats.pending, (state) => {
+				state.status = 'loading';
+			})
+			.addCase(productThunk.getStats.fulfilled, (state, action) => {
+				state.status = 'succeeded';
+				state.stats = action.payload;
+			})
+			.addCase(productThunk.getStats.rejected, (state, action) => {
+				state.status = 'failed';
+				state.error = (action.payload as string) || 'An error occurred';
+			});
+	},
+);
 export const { setCurrentPage, setItemsPerPage } = productSlice.actions;

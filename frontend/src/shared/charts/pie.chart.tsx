d8cf37b26e21cb08e25dce3dcd48c5c80284d127
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-28 23:21:06 -0400

    feat(pie chart): create a pie chart using recharts

---
+++ frontend/src/shared/charts/pie.chart.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { PieChart, Pie, ResponsiveContainer } from 'recharts';
+
+const genColors = (n: number) => Array.from({ length: n }, (_, i) => `hsl(${(360 * i) / n}, 55%, 70%)`);
+
+interface Props {
+	data: {
+		name: string;
+		count: number;
+	}[];
+}
+export function DynamicPieChart({ data }: Props) {
+	const colors = genColors(data?.length);
+	const chartData = data.map((item, index) => ({
+		name: item.name,
+		value: Number(item.count),
+		fill: colors[index],
+	}));
+
+	const renderLabel = ({ cx, cy, midAngle, outerRadius, name, percent }: any) => {
+		const RADIAN = Math.PI / 180;
+		const radius = outerRadius + 25;
+		const x = cx + radius * Math.cos(-midAngle * RADIAN);
+		const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+		return (
+			<text
+				x={x}
+				y={y}
+				fill="#333"
+				textAnchor={x > cx ? 'start' : 'end'}
+				dominantBaseline="central"
+				fontSize={12}
+			>
+				{`${name} (${(percent * 100).toFixed(0)}%)`}
+			</text>
+		);
+	};
+
+	return (
+		<section className="h-[145px] w-[350px] border border-gray-100 rounded-md bg-gray-50">
+			<ResponsiveContainer width={'100%'} height={'100%'}>
+				<PieChart width={'100%'} height={'100%'}>
+					<Pie
+						data={chartData}
+						dataKey="value"
+						nameKey="name"
+						cx="50%"
+						cy="50%"
+						innerRadius={0}
+						outerRadius={40}
+						paddingAngle={0}
+						cornerRadius={0}
+						label={renderLabel}
+					/>
+				</PieChart>
+			</ResponsiveContainer>
+		</section>
+	);
+}

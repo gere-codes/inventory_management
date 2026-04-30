88c140bed991d21621225b4e4162c45d95873eb1
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-04-30 00:04:43 -0400

    feat(categories page): add the dynamic pie chart

--- frontend/src/pages/categories/categories.page.tsx
+++ frontend/src/pages/categories/categories.page.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { selectCategories, selectCategoryPagination, categoryThunk, CategoryTable } from '@categories';
-import { useAppDispatch, useAppSelector } from '@hooks/index';
-import { Pagination, SearchBar } from '@common/index';
+import { useAppDispatch, useAppSelector } from '@hooks';
+import { DynamicPieChart, Pagination, SearchBar } from '@common';
+import { productThunk, selectProductStats } from '@products';
 
 export const CategoriesPage = () => {
 	const [term, setTerm] = useState<string>('');
@@ -11,6 +12,12 @@ export const CategoriesPage = () => {
 
 	const { currentPage, itemsPerPage, totalItems, totalPages } = useAppSelector(selectCategoryPagination);
 
+	const productStats = useAppSelector(selectProductStats);
+
+	useEffect(() => {
+		dispatch(productThunk.getStats());
+	}, [dispatch]);
+
 	useEffect(() => {
 		dispatch(categoryThunk.paginate({ page: currentPage, limit: itemsPerPage }));
 	}, [dispatch, currentPage, itemsPerPage]);
@@ -18,16 +25,20 @@ export const CategoriesPage = () => {
 	const noop = () => {};
 
 	return (
-		<section>
-			<SearchBar value={term} onSearch={(newValue) => setTerm(newValue)} placeholder="Search Product..." />
-			<CategoryTable categories={categories} onDelete={noop} onEdit={noop} />
-			<Pagination
-				currentPage={currentPage}
-				totalPages={totalPages}
-				itemsPerPage={itemsPerPage}
-				onPageChange={noop}
-				onPerPageChange={noop}
-			/>
+		<section className="flex flex-col lg:flex-row">
+			<section className=" w-full lg:max-w-2/3">
+				<SearchBar value={term} onSearch={(newValue) => setTerm(newValue)} placeholder="Search Product..." />
+				<CategoryTable categories={categories} onDelete={noop} onEdit={noop} />
+				<Pagination
+					currentPage={currentPage}
+					totalPages={totalPages}
+					itemsPerPage={itemsPerPage}
+					onPageChange={noop}
+					onPerPageChange={noop}
+				/>
+			</section>
+
+			<DynamicPieChart data={productStats.categories} />
 		</section>
 	);
 };

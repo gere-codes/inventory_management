f4c6861144ff0c728cd4cba2a66739b51ac5bfb5
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-05-03 17:57:08 -0400

    update( product selectors): update the product selectors to match the product  state

--- frontend/src/features/products/product.selectors.ts
+++ frontend/src/features/products/product.selectors.ts
@@ -1,5 +1,5 @@
 import type { RootState } from '@store';
 
-export const selectProducts = (state: RootState) => state.product.items;
+export const selectProducts = (state: RootState) => state.product.list.data;
 export const selectProductsPagination = (state: RootState) => state.product.pagination;
 export const selectProductStats = (state: RootState) => state.product.stats.data;

65d15be6f1bc0eaada5c3b4ea04ec40823e00e1d
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-05-08 15:28:35 -0400

    update(suppliers page): add search bar and the 'add suppliers button'

--- frontend/src/pages/suppliers/suppliers.page.tsx
+++ frontend/src/pages/suppliers/suppliers.page.tsx
@@ -1,10 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { selectSuppliersList, selectSuppliersPagination, supplierThunk } from '@suppliers';
 import { useAppDispatch, useAppSelector } from '@hooks';
 import { SupplierTable } from '@suppliers';
+import { EModalMode, EModalType, openModal, SearchBar } from '@common';
+import { debounce } from '@utils';
+import { Button } from '@/shared/components/ui';
 
 export const SuppliersPage = () => {
+	const FIRST_PAGE = 1;
+	const [term, setTerm] = useState<string>('');
+
 	const suppliers = useAppSelector(selectSuppliersList);
+	const { currentPage, itemsPerPage, totalItems, totalPages } = useAppSelector(selectSuppliersPagination);
 
 	const pagiination = useAppSelector(selectSuppliersPagination);
 	const dispatch = useAppDispatch();
@@ -14,9 +21,46 @@ export const SuppliersPage = () => {
 	}, [pagiination?.currentPage, pagiination?.itemsPerPage, dispatch]);
 
 	const noop = () => {};
+
+	const debouncedSearch = useMemo(
+		() =>
+			debounce((term) => {
+				dispatch(supplierThunk.search({ term, page: FIRST_PAGE, limit: itemsPerPage }));
+			}, 500),
+		[dispatch, itemsPerPage],
+	);
+
+	useEffect(() => {
+		return () => {
+			debouncedSearch.cancel();
+		};
+	}, [debouncedSearch]);
 	return (
 		<section>
-			<h2>section page</h2>
+			{/* Search + Add */}
+			<section className="flex justify-between items-end">
+				<div className="flex flex-col gap-2 flex-1">
+					<h2 className="text-xl font-bold">Suppliers List</h2>
+					<SearchBar
+						value={term}
+						onSearch={(newValue) => {
+							setTerm(newValue);
+							debouncedSearch(newValue);
+						}}
+						placeholder="Search..."
+					/>
+				</div>
+
+				<Button
+					className="w-fit flex items-center gap-1 text-white"
+					style={{ width: 140, height: 40 }}
+					onClick={() =>
+						dispatch(openModal({ data: null, mode: EModalMode.CREATE, type: EModalType.SUPPLIER }))
+					}
+				>
+					+ Add Supplier
+				</Button>
+			</section>
 
 			{/* Table */}
 			<SupplierTable suppliers={suppliers} onDelete={noop} onEdit={noop} />

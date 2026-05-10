236484eb4d698ba6b80f2e20888e808a7d194fc9
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-05-10 16:51:32 -0400

    feat(supsplier form): create supplier form schema and infer its types

--- frontend/src/features/suppliers/supplier.schema.ts
+++ frontend/src/features/suppliers/supplier.schema.ts
@@ -1,3 +1,4 @@
+import { EModalMode } from '@/shared/components/common';
 import z from 'zod';
 
 export const supplierCreateSchema = z.object({
@@ -18,3 +19,28 @@ export const supplierUpdateSchema = supplierCreateSchema.partial();
 export type TSupplierCreate = z.infer<typeof supplierCreateSchema>;
 export type TSupplier = z.infer<typeof supplierSchema>;
 export type TSupplierUpdate = z.infer<typeof supplierUpdateSchema>;
+
+export const supplierFormSchema = z.discriminatedUnion('mode', [
+	z.object({
+		mode: z.literal(EModalMode.CREATE),
+		id: z.uuid().optional(),
+		name: z.string().min(1).max(100),
+		phone: z.string().regex(/^[0-9+\-()/\s]+$/),
+		address: z.string().optional().nullable(),
+		description: z.string().optional().nullable(),
+		createdAt: z.string().optional(),
+		updateAt: z.string().optional(),
+	}),
+	z.object({
+		mode: z.literal(EModalMode.EDIT),
+		id: z.uuid(),
+		name: z.string().min(1).max(100),
+		phone: z.string().regex(/^[0-9+\-()/\s]+$/),
+		address: z.string().optional().nullable(),
+		description: z.string().optional().nullable(),
+		createdAt: z.string().optional(),
+		updateAt: z.string().optional(),
+	}),
+]);
+
+export type TSupplierForm = z.infer<typeof supplierFormSchema>;

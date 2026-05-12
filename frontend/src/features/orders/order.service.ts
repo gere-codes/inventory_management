b8f0cf36c6c2ece1f047d8490fea60ce7444d09b
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-05-12 15:10:16 -0400

    feat(order): create order service

---
+++ frontend/src/features/orders/order.service.ts
@@ -0,0 +1,17 @@
+import { BaseService } from '@/shared/base';
+import {
+	type TCreateOrder,
+	type TUpdateOrder,
+	type TOrder,
+	orderSchema,
+	updateOrderSchema,
+	createOrderSchema,
+} from './order.schema';
+
+class OrderService extends BaseService<TOrder, TCreateOrder, TUpdateOrder> {
+	constructor(resource: string) {
+		super(resource, orderSchema, createOrderSchema, updateOrderSchema);
+	}
+}
+
+export const orderService = new OrderService('order');

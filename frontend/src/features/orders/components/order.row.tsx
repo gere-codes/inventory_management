26f49e44b04aeeaa7fc65bd324d175ecb8adc8f9
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-05-12 14:46:37 -0400

    feat(order row): create order row

---
+++ frontend/src/features/orders/components/order.row.tsx
@@ -0,0 +1,75 @@
+import { memo, type FC } from 'react';
+import { BASE_URL } from '@api';
+import { AiFillEdit } from 'react-icons/ai';
+import { MdDelete } from 'react-icons/md';
+import { IoBagHandleSharp } from 'react-icons/io5';
+import type { IconType } from 'react-icons/lib';
+import type { TOrder } from '../order.schema';
+import { EOrderStatus } from '../order.enums';
+
+interface Props {
+	order: TOrder;
+	onEdit: (order: TOrder) => void;
+	onDelete: (order: TOrder) => void;
+	onOrder: (order: TOrder) => void;
+}
+export const orderItem = memo(({ order, onEdit, onDelete, onOrder }: Props) => {
+	const statusStyles = {
+		[EOrderStatus.RECEIVED]: 'bg-green-100 text-green-800',
+		[EOrderStatus.PENDING]: 'bg-yellow-100 text-yellow-800',
+		[EOrderStatus.CANCELLED]: 'bg-red-100 text-red-800',
+	};
+
+	return (
+		<tr key={order.id} className="hover:bg-gray-50  border-b border-gray-200">
+			<TableItemData item={order.sku} />
+			<TableItemData item={order.name} imageUrl={order?.imageUrl ?? ''} />
+			<TableItemData item={'$' + Number(order.price).toFixed(2)} />
+			<TableItemData item={order.category} />
+			<TableItemData item={`${order.quantity} pcs`} />
+			<td className="px-4 py-4  w-1/7 text-ellipsis overflow-hidden">
+				<span className={`px-3 py-1.5 text-xs truncate font-medium rounded ${statusStyles[order.status]}`}>
+					{order.status}
+				</span>
+			</td>
+			<td className="px-4 py-4  w-1/7 text-ellipsis overflow-hidden">
+				<span className="flex gap-3 truncate ">
+					<TableActionButton handleClick={() => onEdit(order)} Icon={AiFillEdit} />
+					<TableActionButton handleClick={() => onDelete(order)} Icon={MdDelete} />
+					<TableActionButton handleClick={() => onOrder(order)} Icon={IoBagHandleSharp} />
+				</span>
+			</td>
+		</tr>
+	);
+});
+
+const TableItemData = ({ item, imageUrl }: { item: string | number; imageUrl?: string }) => {
+	return (
+		<td className="px-4 py-4  w-1/7 text-ellipsis  overflow-hidden">
+			<div className="flex gap-1 items-center">
+				{imageUrl && (
+					<img
+						src={`${BASE_URL}${imageUrl}`}
+						alt={item.toString().substring(0, 1)}
+						className="w-10 h-10 border border-gray-100 rounded"
+					/>
+				)}
+				<p className="text-sm font-medium text-gray-900 truncate">{item}</p>
+			</div>
+		</td>
+	);
+};
+
+const TableActionButton = ({ handleClick, Icon }: { handleClick: () => void; Icon: IconType }) => {
+	return (
+		<button
+			onClick={(e) => {
+				e.stopPropagation();
+				handleClick();
+			}}
+			className="text-gray-500 "
+		>
+			<Icon size={20} />
+		</button>
+	);
+};
--- frontend/src/features/orders/index.ts
+++ frontend/src/features/orders/index.ts
@@ -1,1 +1,3 @@
+export { orderItem } from './components/order.row';
 export * from './order.schema';
+export * from './order.enums';

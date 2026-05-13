8342d926a36d8075c9b08de740248885c69607b9
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-05-13 10:56:33 -0400

    feat(orders page): add debounce to the order search by term

--- frontend/src/features/orders/order.selector.ts
+++ frontend/src/features/orders/order.selector.ts
@@ -1,4 +1,4 @@
 import type { RootState } from '@store';
 
-export const selectOrderList = (state: RootState) => state.order.list;
+export const selectOrderList = (state: RootState) => state.order.list.data;
 export const selectOrderPagination = (state: RootState) => state.order.pagination;
--- frontend/src/pages/orders/order.page.tsx
+++ frontend/src/pages/orders/order.page.tsx
@@ -1,7 +1,59 @@
+import { useEffect, useMemo, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '@hooks';
+import { debounce } from '@utils';
+import { Button } from '@ui';
+import { EModalMode, EModalType, openModal, SearchBar } from '@common';
+import { OrderTable, orderThunk, selectOrderList, selectOrderPagination } from '@orders';
+
 export const OrdersPage = () => {
+	const FIRST_PAGE = 1;
+	const [term, setTerm] = useState<string>('');
+
+	const { currentPage, itemsPerPage, totalItems, totalPages } = useAppSelector(selectOrderPagination);
+	const orders = useAppSelector(selectOrderList);
+	const dispatch = useAppDispatch();
+
+	const noop = () => {};
+
+	const debouncedSearch = useMemo(
+		() =>
+			debounce((term) => {
+				dispatch(orderThunk.search({ term, page: FIRST_PAGE, limit: itemsPerPage }));
+			}, 500),
+		[dispatch, itemsPerPage],
+	);
+
+	useEffect(() => {
+		return () => {
+			debouncedSearch.cancel();
+		};
+	}, [debouncedSearch]);
+
 	return (
-		<div>
-			<h2>orders page</h2>
-		</div>
+		<section>
+			{/* Search + Add */}
+			<section className="flex justify-between items-end">
+				<div className="flex flex-col gap-2 flex-1">
+					<h2 className="text-xl font-bold">Orders List</h2>
+					<SearchBar
+						value={term}
+						onSearch={(newValue) => {
+							setTerm(newValue);
+							debouncedSearch(newValue);
+						}}
+						placeholder="Search..."
+					/>
+				</div>
+
+				<Button
+					className="w-fit flex items-center gap-1 text-white"
+					style={{ width: 138, height: 40 }}
+					onClick={() => dispatch(openModal({ data: null, mode: EModalMode.CREATE, type: EModalType.ORDER }))}
+				>
+					+ Add Order
+				</Button>
+			</section>
+			<OrderTable orders={orders} onDelete={noop} onEdit={noop} onOrder={noop} />
+		</section>
 	);
 };

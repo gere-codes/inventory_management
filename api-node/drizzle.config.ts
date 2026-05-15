ee10e007e1696e08b821d6da0a27b3b982ed883a
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-05-14 21:09:08 -0400

    update(drizzle): clean up drizzle migration files and regenerate snapshots

--- api-node/drizzle.config.ts
+++ api-node/drizzle.config.ts
@@ -2,12 +2,12 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
+const url = process.env.DATABASE_URL;
+if (!url) throw new Error('DATABASE_URL is not defined');
+
 export default defineConfig({
 	schema: './src/db/schema/index.ts',
 	out: './drizzle',
 	dialect: 'postgresql',
-
-	dbCredentials: {
-		url: process.env.DATABASE_URL as string,
-	},
+	dbCredentials: { url },
 });
--- api-node/drizzle/0000_chilly_justice.sql
+++ api-node/drizzle/0000_chilly_justice.sql
@@ -1,10 +1,28 @@
 CREATE TABLE "categories" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"user_id" uuid NOT NULL,
-	"name" varchar(10) NOT NULL,
+	"name" varchar(100) NOT NULL,
 	"description" varchar(225),
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "orders" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"user_id" uuid,
+	"description" varchar(1000),
+	"product_id" uuid,
+	"name" varchar(100) NOT NULL,
+	"sku" varchar(36) NOT NULL,
+	"price" numeric DEFAULT '0.00' NOT NULL,
+	"quantity" integer DEFAULT 1 NOT NULL,
+	"category_id" uuid NOT NULL,
+	"image_url" varchar(500),
+	"status" varchar(20) DEFAULT 'pending' NOT NULL,
+	"type" varchar(20) DEFAULT 'new' NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now(),
+	CONSTRAINT "orders_sku_unique" UNIQUE("sku")
 );
 --> statement-breakpoint
 CREATE TABLE "products" (
@@ -16,6 +34,7 @@ CREATE TABLE "products" (
 	"price" numeric DEFAULT '0.00' NOT NULL,
 	"quantity" integer DEFAULT 1 NOT NULL,
 	"category_id" uuid NOT NULL,
+	"image_url" varchar(500),
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now(),
 	CONSTRAINT "products_sku_unique" UNIQUE("sku")
@@ -32,7 +51,20 @@ CREATE TABLE "suppliers" (
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(100) NOT NULL,
+	"email" varchar(100) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
 ALTER TABLE "categories" ADD CONSTRAINT "categories_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "orders" ADD CONSTRAINT "orders_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "orders" ADD CONSTRAINT "orders_product_id_products_id_fk" FOREIGN KEY ("product_id") REFERENCES "public"."products"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "orders" ADD CONSTRAINT "orders_category_id_categories_id_fk" FOREIGN KEY ("category_id") REFERENCES "public"."categories"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "products" ADD CONSTRAINT "products_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "products" ADD CONSTRAINT "products_category_id_categories_id_fk" FOREIGN KEY ("category_id") REFERENCES "public"."categories"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "suppliers" ADD CONSTRAINT "suppliers_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;

6133cc703eb2bcf787b00aa7b33c67856f50f6f5
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-05-23 15:28:25 -0400

    update(product table): replace the image column to images and change the type to array

--- api-node/src/db/schema/product.ts
+++ api-node/src/db/schema/product.ts
@@ -1,6 +1,7 @@
-import { pgTable, uuid, varchar, timestamp, decimal, integer } from 'drizzle-orm/pg-core';
+import { pgTable, uuid, varchar, timestamp, decimal, integer, text } from 'drizzle-orm/pg-core';
 import { users } from './user.js';
 import { categories } from './category.js';
+import { sql } from 'drizzle-orm';
 
 export const products = pgTable('products', {
 	id: uuid('id').primaryKey().defaultRandom(),
@@ -13,7 +14,12 @@ export const products = pgTable('products', {
 	categoryId: uuid('category_id')
 		.references(() => categories.id)
 		.notNull(),
-	image: varchar('image', { length: 500 }),
+	// image: varchar('image', { length: 500 }),
+	images: text('images')
+		.array()
+		.notNull()
+		.default(sql`'{}'::text[]`),
+
 	createdAt: timestamp('created_at').defaultNow().notNull(),
 	updatedAt: timestamp('updated_at')
 		.defaultNow()

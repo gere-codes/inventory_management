9d6d1ac9ef7267a854805235ed23ebbbc8621077
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-05-27 18:52:06 -0400

    feat(category route): add the multer middleware to handle category image

--- api-node/src/modules/category/category.route.ts
+++ api-node/src/modules/category/category.route.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { categoryController } from './category.controller.js';
+import { upload } from '@middlewares';
 
 const categoryRoutes = Router();
 
 categoryRoutes.get('/', categoryController.getAll);
 categoryRoutes.get('/paginate', categoryController.paginate);
 categoryRoutes.get('/search', categoryController.search);
-categoryRoutes.post('/', categoryController.create);
-categoryRoutes.put('/:id', categoryController.update);
+categoryRoutes.post('/', upload.single('image'), categoryController.create);
+categoryRoutes.put('/:id', upload.single('image'), categoryController.update);
 categoryRoutes.delete('/:id', categoryController.delete);
 
 categoryRoutes.get('/:id', categoryController.getById);

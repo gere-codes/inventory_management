8135333924c48832c4ea4925ad87f98c4f80fc46
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-06-01 20:43:48 -0400

    feat(general schema): create base query schema

---
+++ frontend/src/shared/schema/general.schema.ts
@@ -0,0 +1,26 @@
+import z from 'zod';
+
+export const commonQuery = z.object({
+	isPaginated: z.boolean().default(true),
+	page: z.coerce.number().int().positive().default(1),
+	limit: z.coerce.number().int().positive().max(50).default(10),
+	order: z.string().default('desc'),
+	search: z.string().optional(),
+});
+export const baseQuerySchema = commonQuery.transform((raw) => ({
+	isPaginated: raw.isPaginated,
+	pagination: {
+		page: raw.page,
+		limit: raw.limit,
+		offset: (raw.page - 1) * raw.limit,
+	},
+	filter: {
+		search: raw.search,
+	},
+	sort: {
+		field: 'createdAt',
+		order: raw.order,
+	},
+}));
+
+export type TBaseQuery = z.infer<typeof baseQuerySchema>;

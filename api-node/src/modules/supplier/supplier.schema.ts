a0283baacb22899b0e454169fe37aa68b44bf37c
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-06-01 21:22:49 -0400

    feat(supplier schema): define and transform the supplier schema

--- api-node/src/modules/supplier/supplier.schema.ts
+++ api-node/src/modules/supplier/supplier.schema.ts
@@ -1,3 +1,4 @@
+import { commonQuery } from '@src/core/schema/general.schema.js';
 import { sanitized, sanitizedPhone } from '@src/core/validation/sanitized.js';
 import z from 'zod';
 
@@ -19,3 +20,27 @@ export const supplierUpdateSchema = supplierCreateSchema.partial();
 export type TSupplierCreate = z.infer<typeof supplierCreateSchema>;
 export type TSupplier = z.infer<typeof supplierSchema>;
 export type TSupplierUpdate = z.infer<typeof supplierUpdateSchema>;
+
+export const supplierQuerySchema = commonQuery
+	.extend({
+		search: z.string().optional(),
+		sort: z.enum(['createdAt', 'name']).default('createdAt'),
+		order: z.enum(['asc', 'desc']).default('desc'),
+	})
+	.transform((raw) => ({
+		isPaginated: raw.isPaginated,
+		pagination: {
+			page: raw.page,
+			limit: raw.limit,
+			offset: (raw.page - 1) * raw.limit,
+		},
+		filter: {
+			search: raw.search,
+		},
+		sort: {
+			field: raw.sort,
+			order: raw.order,
+		},
+	}));
+
+export type TSupplierQuery = z.infer<typeof supplierQuerySchema>;

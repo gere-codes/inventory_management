0156a014ad3c32f4dafbd61d619e562665ad1303
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-06-01 21:00:31 -0400

    feat(product schema): define and transform them product query schema

--- api-node/src/modules/product/product.shema.ts
+++ api-node/src/modules/product/product.shema.ts
@@ -1,6 +1,7 @@
 import { sanitized } from '@src/core/validation/sanitized.js';
 import { z } from 'zod';
 import { EProductStatus } from './product.enum.js';
+import { baseQuerySchema, commonQuery } from '@src/core/schema/general.schema.js';
 
 export const productCreateSchema = z.object({
 	name: sanitized(z.string().min(2).max(100)),
@@ -24,3 +25,27 @@ export const productUpdateSchema = productCreateSchema.partial();
 export type TProduct = z.infer<typeof productSchema>;
 export type TProductCreate = z.infer<typeof productCreateSchema>;
 export type TProductUpdate = z.infer<typeof productUpdateSchema>;
+
+export const productQuerySchema = commonQuery
+	.extend({
+		categoryId: z.uuid().optional(),
+		sort: z.enum(['createdAt', 'price']).default('createdAt'),
+	})
+	.transform((raw) => ({
+		isPaginated: raw.isPaginated,
+		pagination: {
+			page: raw.page,
+			limit: raw.limit,
+			offset: (raw.page - 1) * raw.limit,
+		},
+		filter: {
+			search: raw.search,
+			categoryId: raw.categoryId,
+		},
+		sort: {
+			field: raw.sort,
+			order: raw.order,
+		},
+	}));
+
+export type TProductQuery = z.infer<typeof productQuerySchema>;

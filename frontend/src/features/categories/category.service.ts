253dbe8a5d9fa79aceed2c192ef6e858aa60eed8
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-06-02 14:57:47 -0400

    update(category service): inject categoryQuerySchema to the category service

--- frontend/src/features/categories/category.service.ts
+++ frontend/src/features/categories/category.service.ts
@@ -1,16 +1,25 @@
 import { BaseService } from '@base';
 import {
 	categoryCreateSchema,
+	categoryQuerySchema,
 	categorySchema,
 	categoryUpdateSchema,
 	type TCategory,
 	type TCategoryCreate,
+	type TCategoryQuery,
 	type TCategoryUpdate,
 } from './category.schema';
 
-export class CategoryService extends BaseService<TCategory, TCategoryCreate, TCategoryUpdate, FormData, FormData> {
+export class CategoryService extends BaseService<
+	TCategory,
+	TCategoryCreate,
+	TCategoryUpdate,
+	FormData,
+	FormData,
+	TCategoryQuery
+> {
 	constructor(resource: string) {
-		super(resource, categorySchema, categoryCreateSchema, categoryUpdateSchema);
+		super(resource, categorySchema, categoryCreateSchema, categoryUpdateSchema, categoryQuerySchema);
 	}
 }
 

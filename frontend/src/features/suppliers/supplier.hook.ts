5ebc851e13f0abf824eab9a1315a800e7b9f21cb
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-06-09 19:38:43 -0400

    feat(supplier hook): create useSupplierFilter hook

--- frontend/src/features/suppliers/supplier.hook.ts
+++ frontend/src/features/suppliers/supplier.hook.ts
@@ -1,7 +1,7 @@
-import type { TSupplier } from './supplier.schema';
-import { selectSuppliersList, selectSuppliersPagination } from './supplier.selectors';
+import { suppplierQuerySchema, type TSupplier } from './supplier.schema';
+import { selectSuppliersList, selectSuppliersPagination, selectSuppliersStatus } from './supplier.selectors';
 import { closeModal } from '@common';
-import { useAppDispatch, useAppSelector } from '@hooks';
+import { useAppDispatch, useAppSelector, useCollectionFilter } from '@hooks';
 import { supplierThunk } from './supplier.thunk';
 
 export const useSupplierManager = (supplierData: TSupplier) => {
@@ -16,4 +16,44 @@ export const useSupplierManager = (supplierData: TSupplier) => {
 	};
 
 	return { handleDelete, close };
+};
+
+export const useSupplierFilter = () => {
+	const {
+		filters,
+		setLimit,
+		setPage,
+		fetchData,
+		searchTerm,
+		handleSearchChange,
+		pagination,
+		resetFilters,
+		handlePageChange,
+		handleLimitChange,
+		setParam,
+		status,
+		data,
+	} = useCollectionFilter({
+		schema: suppplierQuerySchema,
+		thunkAction: supplierThunk.getCollection,
+		selectData: selectSuppliersList,
+		selectPagination: selectSuppliersPagination,
+		selectStatus: selectSuppliersStatus,
+	});
+
+	return {
+		filters,
+		setLimit,
+		setPage,
+		fetchData,
+		searchTerm,
+		handlePageChange,
+		handleLimitChange,
+		handleSearchChange,
+		pagination,
+		resetFilters,
+		setParam,
+		status,
+		data,
+	};
 };
--- frontend/src/features/suppliers/supplier.selectors.ts
+++ frontend/src/features/suppliers/supplier.selectors.ts
@@ -2,3 +2,4 @@ import type { RootState } from '@store';
 
 export const selectSuppliersList = (state: RootState) => state.supplier.list.data;
 export const selectSuppliersPagination = (state: RootState) => state.supplier.pagination;
+export const selectSuppliersStatus = (state: RootState) => state.supplier.list.status;

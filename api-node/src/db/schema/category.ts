072b09c714f4eae011b26ac4e9a1a81617b1f4a4
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-06-12 23:26:12 -0400

    feat(category table): add the slug column and index it

--- api-node/src/db/schema/category.ts
+++ api-node/src/db/schema/category.ts
@@ -1,21 +1,29 @@
-import { pgTable, uuid, varchar, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, uuid, varchar, timestamp, uniqueIndex } from 'drizzle-orm/pg-core';
 import { users } from './user.js';
-import { text } from 'drizzle-orm/gel-core';
 
-export const categories = pgTable('categories', {
-	id: uuid('id').primaryKey().defaultRandom(),
-	userId: uuid('user_id')
-		.references(() => users.id)
-		.notNull(),
-	name: varchar('name', { length: 100 }).notNull(),
-	image: varchar('image', { length: 500 }).notNull(),
-	description: varchar('description', { length: 225 }),
-	createdAt: timestamp('created_at').defaultNow().notNull(),
-	updatedAt: timestamp('updated_at')
-		.defaultNow()
-		.notNull()
-		.$onUpdate(() => new Date()),
-});
+export const categories = pgTable(
+	'categories',
+	{
+		id: uuid('id').primaryKey().defaultRandom(),
+		userId: uuid('user_id')
+			.references(() => users.id)
+			.notNull(),
+		name: varchar('name', { length: 100 }).notNull(),
+		image: varchar('image', { length: 500 }).notNull(),
+		slug: varchar('slug', { length: 100 }).notNull().unique(),
+		description: varchar('description', { length: 225 }),
+		createdAt: timestamp('created_at').defaultNow().notNull(),
+		updatedAt: timestamp('updated_at')
+			.defaultNow()
+			.notNull()
+			.$onUpdate(() => new Date()),
+	},
+	(table) => {
+		return {
+			slugIdx: uniqueIndex('slug_idx').on(table.slug),
+		};
+	},
+);
 
 export type TCategory = typeof categories.$inferSelect;
 export type TCategoryCreate = typeof categories.$inferInsert;

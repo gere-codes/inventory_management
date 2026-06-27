cb92a93b56dd344239c8068b3781cff4e212fdf4
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-06-26 20:53:15 -0400

    update: add the vite base url to the docker env variable

--- frontend/src/shared/api/instance.api.ts
+++ frontend/src/shared/api/instance.api.ts
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
-export const BASE_URL = import.meta.env.VITE_BASE || 'http://localhost:5000';
+export const BASE_URL = import.meta.env.VITE_BASE || 'http://localhost:5000/api';
 
 // public instance
 export const publicInstance = axios.create({
-	baseURL: `${BASE_URL}/api`,
+	baseURL: `${BASE_URL}`,
 	withCredentials: true,
 });
 
 // private instance
 export const privateInstance = axios.create({
-	baseURL: `${BASE_URL}/api`,
+	baseURL: `${BASE_URL}`,
 	withCredentials: true,
 });
 
 // refresh token instance
 export const refreshClient = axios.create({
-	baseURL: `${BASE_URL}/api`,
+	baseURL: `${BASE_URL}`,
 	withCredentials: true,
 });

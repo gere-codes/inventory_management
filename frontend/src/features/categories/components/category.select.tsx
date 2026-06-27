9ec6d2d794126cc68c87378dc157c344488b5e5d
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-06-26 20:17:15 -0400

    update(category select): change the category register type to generic

--- frontend/src/features/categories/components/category.select.tsx
+++ frontend/src/features/categories/components/category.select.tsx
@@ -1,15 +1,15 @@
-import type { UseFormRegister } from 'react-hook-form';
+import type { FieldValues, Path, UseFormRegister } from 'react-hook-form';
 import type { TCategory } from '../category.schema';
 import type { TProductFormValues } from '@products';
 
-interface Props {
+interface Props<T extends FieldValues> {
 	categories: TCategory[];
 	isLoading: boolean;
-	register: UseFormRegister<TProductFormValues>;
+	register: UseFormRegister<T>;
 	disabled?: boolean;
 }
 
-export const CategorySelect = ({ categories, isLoading, register, disabled }: Props) => {
+export const CategorySelect = <T extends FieldValues>({ categories, isLoading, register, disabled }: Props<T>) => {
 	if (isLoading) return <div className="animate-pulse w-full h-[42px] bg-gray-200 rounded" />;
 
 	return (
@@ -19,7 +19,7 @@ export const CategorySelect = ({ categories, isLoading, register, disabled }: Pr
 			</label>
 			<select
 				required
-				{...register('categoryId')}
+				{...register('categoryId' as Path<T>)}
 				className="w-full px-3 py-2 border border-gray-300 rounded-md h-[42px]"
 				disabled={disabled}
 			>

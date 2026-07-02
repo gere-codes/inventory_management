779eebadbc9f99098b4e3f0a03c35a568cb2d145
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-07-02 15:38:31 -0400

    update(product stats): show skeleton while the the products stats is loading

--- frontend/src/features/products/components/product.stats.tsx
+++ frontend/src/features/products/components/product.stats.tsx
@@ -7,7 +7,7 @@ import type { TProductStats } from '../product.schema';
 export const ProductsStats = () => {
 	const { data, isLoading } = useProductStats();
 
-	if (isLoading) {
+	if (isLoading || !data) {
 		// skeleton
 		return <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 flex-1"></section>;
 	}

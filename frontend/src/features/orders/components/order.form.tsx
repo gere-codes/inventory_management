fe0ac9a76ae45388cdf8a0e325b61fbb00edefc0
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-07-16 17:35:25 -0400

    refactor(order form): ensure images are appended with array notation

--- frontend/src/features/orders/components/order.form.tsx
+++ frontend/src/features/orders/components/order.form.tsx
@@ -61,14 +61,10 @@ export const OrderForm = ({ mode, orderData }: Props) => {
 
 			if (Array.isArray(value)) {
 				value.forEach((item) => {
-					if (item instanceof File) {
-						formData.append(key, item);
-					} else if (typeof item === 'string') {
-						formData.append(key, item);
-					}
+					formData.append(`${key}[]`, item);
 				});
 			} else {
-				formData.append(key, value.toString());
+				formData.append(key, String(value));
 			}
 		});
 

190d28023139f1d07e4c045e885bfce8b756e555
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-09-07 18:36:14 -0400

    feat(auth controller): Add the Register controller

---
+++ api-dotnet/Modules/Auth/AuthController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/auth/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<AuthResponseDTO>> Register([FromBody] RegisterRequestDTO registerData)
+    {
+        var result = await _authService.RegisterAsync(registerData);
+        var cookieOptions = new CookieOptions
+        {
+            HttpOnly = true,                
+            Secure = true,                  
+            SameSite = SameSiteMode.Strict,
+            Expires = DateTime.UtcNow.AddDays(7) 
+        };
+
+        Response.Cookies.Append("refreshToken", result.RefreshToken, cookieOptions);
+
+        var clientResponse = new ClientAuthResponseDto(result.AccessToken, result.User);
+
+        return Ok(clientResponse);
+        
+    }
+}

fb355922df21967ff1a218c7c040c5d125b38869
gere-dev <32871055+gere-dev@users.noreply.github.com>
2026-09-07 18:31:14 -0400

    feat(program): add DbContext and AuthRepository configuration to the application services

--- api-dotnet/Program.cs
+++ api-dotnet/Program.cs
@@ -1,6 +1,44 @@
+
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString)); 
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseExceptionHandler();
+
+app.MapGet("/health", () => "I'm healthy!");
+app.MapControllers();
+
+
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    try
+    {
+        await db.Database.OpenConnectionAsync();
+        Console.WriteLine("Database connection successful!");
+        await db.Database.CloseConnectionAsync();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Database connection failed!");
+        Console.WriteLine(ex.Message);
+    }
+}
+
 
 app.Run();
